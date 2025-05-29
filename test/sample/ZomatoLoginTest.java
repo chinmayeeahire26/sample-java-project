4c03ac9b5d4ed2b87c066622e048e15fe0e73415
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-05-29 15:25:53 +0530

    updated test/sample/ZomatoLoginTest.java

--- test/sample/ZomatoLoginTest.java
+++ test/sample/ZomatoLoginTest.java
@@ -19,6 +19,27 @@ public class ZomatoLoginTest {
         driver.get("https://www.zomato.com");
     }
 
+    @Testimport org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ZomatoLoginTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.zomato.com");
+    }
+
     @Test
     public void loginToZomato() {
         WebElement loginButton = driver.findElement(By.id("login-button-id"));
@@ -36,6 +57,13 @@ public class ZomatoLoginTest {
         WebElement profileName = driver.findElement(By.id("profile-name-id"));
         Assert.assertEquals(profileName.getText(), "Expected Profile Name");
     }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
 
     @AfterMethod
     public void tearDown() {
