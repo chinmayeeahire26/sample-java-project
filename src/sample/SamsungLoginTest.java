df32a8f212af8801dec7e6e4e638350bae54fc68
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-05-29 11:28:31 +0530

    updated src/sample/SamsungLoginTest.java

---
+++ src/sample/SamsungLoginTest.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SamsungLoginTest {
+
+    @Test
+    public void loginToSamsung() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.samsung.com/login");
+
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+
+        usernameField.sendKeys("yourUsername");
+        passwordField.sendKeys("yourPassword");
+        loginButton.click();
+
+        // Add assertions to verify successful login
+        // Example: Assert.assertTrue(driver.findElement(By.id("welcomeMessage")).isDisplayed());
+
+        driver.quit();
+    }
+
+}
