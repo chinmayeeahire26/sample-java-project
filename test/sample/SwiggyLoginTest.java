84752ed596fc686ad75c67b2fd03515f09ad3700
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-05-29 15:35:13 +0530

    updated test/sample/SwiggyLoginTest.java

--- test/sample/SwiggyLoginTest.java
+++ test/sample/SwiggyLoginTest.java
@@ -21,6 +21,29 @@ public class SwiggyLoginTest {
         driver = new ChromeDriver();
     }
 
+    @Testpackage com.swiggy.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class SwiggyLoginTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+    }
+
     @Test
     public void loginToSwiggy() {
         driver.get("https://www.swiggy.com");
@@ -33,6 +56,13 @@ public class SwiggyLoginTest {
         WebElement submitButton = driver.findElement(By.id("submitButtonId"));
         submitButton.click();
     }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
 
     @AfterClass
     public void tearDown() {
