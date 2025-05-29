f1b26be0a5d1df2e899a55c7ab8185d0226f65c8
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-05-29 13:05:26 +0530

    updated test/sample/java/project/SamsungLogoutTest.java

---
+++ test/sample/java/project/SamsungLogoutTest.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import io.cucumber.java.en.When;
+
+public class SamsungLogoutTest {
+
+    private WebDriver driver;
+
+    @Test
+    @When("^user logs out from the Samsung application$")
+    public void logoutFromSamsungApp() {
+        // Navigate to the logout page
+        driver.findElement(By.id("logoutButton")).click();
+        // Verify logout success
+        boolean isLoggedOut = driver.findElement(By.id("loginPage")).isDisplayed();
+        assert isLoggedOut : "Logout failed";
+    }
+
+}
