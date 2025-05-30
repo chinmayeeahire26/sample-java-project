ab16ae01c1cde21ff580bc86e9473b58966523c9
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-05-30 12:11:09 +0530

    updated src/LoginTest.java

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -18,6 +18,26 @@ public class LoginTest {
         driver.manage().window().maximize();
     }
 
+    @Testimport org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
     @Test
     public void loginTest() {
         driver.get("http://example.com/login");
@@ -33,6 +53,15 @@ public class LoginTest {
         WebElement logoutButton = driver.findElement(By.id("logoutButton"));
         Assert.assertTrue(logoutButton.isDisplayed(), "Login failed");
     }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
 
     @AfterClass
     public void tearDown() {
