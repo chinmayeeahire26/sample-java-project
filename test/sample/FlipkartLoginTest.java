a180d63a86a1c89a55413f6b1658333bd4730f71
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-05-30 11:33:46 +0530

    updated test/sample/FlipkartLoginTest.java

--- test/sample/FlipkartLoginTest.java
+++ test/sample/FlipkartLoginTest.java
@@ -8,6 +8,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+public class FlipkartLoginTest {import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FlipkartLoginTest {
 
     public void loginToFlipkart() {
@@ -40,5 +45,6 @@ public class FlipkartLoginTest {
             driver.quit();
         }
     }
+}
 }
 }
