69e21c2b1545b24240ca06d308e2df7efddf8168
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-05-30 14:54:38 +0530

    updated test/sample/LoginTest.java

---
+++ test/sample/LoginTest.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    public void loginToApplication() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://example.com/login");
+
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+
+        usernameField.sendKeys("testUser");
+        passwordField.sendKeys("testPassword");
+        loginButton.click();
+
+        // Add assertions to verify successful login
+        // Example: Assert.assertTrue(driver.findElement(By.id("logoutButton")).isDisplayed());
+
+        driver.quit();
+    }
+}
