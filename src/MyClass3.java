f0c8a820fa43f90d5effca9aacb9f00d8544c7b8
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-02 17:31:39 +0530

    update MyClass3.java

--- src/MyClass3.java
+++ src/MyClass3.java
@@ -1,5 +1,3 @@
-public class MyClass3 {
-    public void myMethod() {
-        System.out.println(\"updated again today hello github\");  
-    }
+public class MyClass3 {public void myMethod2() {
+        System.out.println("updated again today hello github2"); }
 }
