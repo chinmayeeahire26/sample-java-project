e85cdb75114b7d730be3dadbc6e9527fc254b3b1
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-03 10:59:27 +0530

    updated src/java/LoginTest.java

---
+++ src/java/LoginTest.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://example.com/login");
+    }
+
+    @Test
+    public void loginTest() {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+
+        usernameField.sendKeys("testuser");
+        passwordField.sendKeys("password");
+        loginButton.click();
+
+        // Add assertions to verify login success
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
