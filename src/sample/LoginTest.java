a2639cc62648888dac30779337b2a986396f95fa
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-03 11:24:40 +0530

    updated src/sample/LoginTest.java

--- src/sample/LoginTest.java
+++ src/sample/LoginTest.java
@@ -1,12 +1,3 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
 public class LoginTest {
 
     private WebDriver driver;
@@ -15,53 +6,19 @@ public class LoginTest {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    @Testimport org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-public class LoginTest {
-
-    private WebDriver driver;
-
-    @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
     }
 
     @Test
     public void loginTest() {
-        driver.get("http://example.com/login");
-
-        WebElement usernameField = driver.findElement(By.id("username"));
-        WebElement passwordField = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.id("loginButton"));
-
-        usernameField.sendKeys("validUsername");
-        passwordField.sendKeys("validPassword");
-        loginButton.click();
-
-        WebElement logoutButton = driver.findElement(By.id("logoutButton"));
-        Assert.assertTrue(logoutButton.isDisplayed(), "Login failed");
+        driver.get("https://www.kinley.com/login");
+        // Implement login steps here
+        // Example: driver.findElement(By.id("username")).sendKeys("validUsername");
+        // Example: driver.findElement(By.id("password")).sendKeys("validPassword");
+        // Example: driver.findElement(By.id("loginButton")).click();
+        
+        // Verify login success
+        assertTrue(driver.getTitle().contains("Dashboard"));
     }
-
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-}
-
 
     @AfterClass
     public void tearDown() {
