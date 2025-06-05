93489cba90cf0ac2dd314c4fa7527c151a6b93eb
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-05 13:28:10 +0530

    updates MyClass5.java

--- src/java/MyClass5.java
+++ src/java/MyClass5.java
@@ -1,5 +1,16 @@
 public class MyClass5 {
     public void myMethod() {
-        System.out.println("hello github updated");
+        String message = getGreetingMessage();
+        System.out.println(message);
+        String goodbye = getGoodbyeMessage();
+        System.out.println(goodbye);
+    }
+
+    public String getGreetingMessage() {
+        return "hello github";
+    }
+
+    private String getGoodbyeMessage() {
+        return "goodbye github";
     }
 }
