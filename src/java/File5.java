0f899c892bc3e9e15e31f553af7f8b70eed7ba8a
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-09 23:37:39 +0530

    Updated multiple methods with helpers

--- src/java/File5.java
+++ src/java/File5.java
@@ -1,12 +1,18 @@
 public class File5 {
 
-    public String methodA() {
-        return "ok";
+    private String methodA() {
+        return helperX();
     }
 
     public String methodX() {
         return "something else";
     }
 
-   
+    public String helperX() {
+        return helpnew();
+    }
+
+    private String helpnew() {
+        return "new2";
+    }
 }
