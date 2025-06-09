9b954716eb473f2a9aa9392ee091ab74946afdd5
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-09 23:31:33 +0530

    Create File5.java

---
+++ src/java/File5.java
@@ -0,0 +1,14 @@
+public class File5 {
+
+    public String methodA() {
+        return helperX();
+    }
+
+    public String methodX() {
+        return "something else";
+    }
+
+    private String helperX() {
+        return "from X new1";
+    }
+}
