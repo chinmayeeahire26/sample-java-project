a865b0992a173113ea7f0893a9e3861ee9ec841e
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-09 15:09:24 +0530

    Update File4.java

--- src/java/File4.java
+++ src/java/File4.java
@@ -8,11 +8,5 @@ public class File4 {
         return help();
     }
 
-    private String help() {
-        return "from X new1";
-    }
-
-    private String helperX() {
-        return "from X new1";
-    }
+  
 }
