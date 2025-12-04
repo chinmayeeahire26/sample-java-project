460cf5c2b102bd882f0c7410ca1b76fbc8cbe4bd
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-12-04 15:07:27 +0530

    updated src/LoginTest.java

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -1,72 +1,20 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
-public class LoginTest {
-
-    private WebDriver driver;
-
-    @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    @Testimport org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-public class LoginTest {
-
-    private WebDriver driver;
-
-    @BeforeClass
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
-
-    @Test
-    public void loginTest() {
-        driver.get("http://example.com/login");
-
-        WebElement usernameField = driver.findElement(By.id("username"));
-        WebElement passwordField = driver.findElement(By.id("password"));
-        WebElement loginButton = driver.findElement(By.id("loginButton"));
-
-        usernameField.sendKeys("validUsername");
-        passwordField.sendKeys("validPassword");
-        loginButton.click();
-
-        WebElement logoutButton = driver.findElement(By.id("logoutButton"));
-        Assert.assertTrue(logoutButton.isDisplayed(), "Login failed");
-    }
-
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+async function loginToFlipkart() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get('https://www.flipkart.com');
+        await driver.wait(until.elementLocated(By.css('input._2IX_2-')), 10000);
+        let usernameField = await driver.findElement(By.css('input._2IX_2-'));
+        let passwordField = await driver.findElement(By.css('input._3mctLh'));
+        let loginButton = await driver.findElement(By.css('button._2KpZ6l'));
+        await usernameField.sendKeys('your-username');
+        await passwordField.sendKeys('your-password');
+        await loginButton.click();
+        await driver.wait(until.titleIs('Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!'), 10000);
+    } finally {
+        await driver.quit();
     }
 }
 
-
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-}
+loginToFlipkart();
