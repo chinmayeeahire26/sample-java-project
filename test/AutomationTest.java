099af3c601a7152e198a50a5c1502bf6f6117b74
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-01-23 17:30:31 +0530

    updated test/AutomationTest.java

--- test/AutomationTest.java
+++
@@ -1,9 +0,0 @@
-describe('Login Test', () => {
-  it('should log in successfully', () => {
-    cy.visit('/login');
-    cy.get('#username').type('testuser');
-    cy.get('#password').type('password123');
-    cy.get('#loginButton').click();
-    cy.url().should('include', '/dashboard');
-  });
-});
