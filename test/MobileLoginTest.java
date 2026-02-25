4610635917026ea1991e0966deaf5a29e9b11b9a
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-02-25 12:08:21 +0530

    updated test/MobileLoginTest.java

--- test/MobileLoginTest.java
+++ test/MobileLoginTest.java
@@ -1,37 +1,10 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-public class MobileLoginTest {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-
-    public MobileLoginTest(WebDriver driver) {
-        this.driver = driver;
-        this.wait = new WebDriverWait(driver, 10);
-    }
-
-    public void loginTest() {
-        // Navigate to login page
-        driver.get("https://example.com/login");
-
-        // Locate username field and enter username
-        WebElement usernameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("username")));
-        usernameField.sendKeys("testuser");
-
-        // Locate password field and enter password
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.sendKeys("password123");
-
-        // Locate and click login button
-        WebElement loginButton = driver.findElement(By.id("loginButton"));
-        loginButton.click();
-
-        // Verify login success
-        WebElement successMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("successMessage")));
-        assert successMessage.getText().contains("Welcome, testuser");
-    }
-}
+describe('User Login Tests', () => {
+  it('should log in with valid credentials', () => {
+    cy.visit('/login');
+    cy.get('input[name="username"]').type('testUser');
+    cy.get('input[name="password"]').type('testPassword');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/dashboard');
+    cy.get('.welcome-message').should('contain', 'Welcome, testUser');
+  });
+});
